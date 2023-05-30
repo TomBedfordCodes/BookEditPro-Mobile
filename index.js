187257a85f0c60024ce88941a688e66ed52bfa28
TomBCodes <thomas.bedford89@gmail.com>
2023-05-30 23:32:14 +0800

    Implement basic DB interaction

--- index.js
+++ index.js
@@ -10,22 +10,50 @@ const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const projectsInDB = ref(database, "projects")
 
-// elements
+onValue(projectsInDB, function(snapshot) {
+    clearProjectListEl()
+    if (!snapshot.exists()) {
+        return
+    }
+    addProjectsToListEl(Object.values(snapshot.val()))
+}) 
+
+
+// HTML elements
 const projnameInputEl = document.getElementById("projname-input")
 const addBtn = document.getElementById("add-btn")
 const projectsListEl = document.getElementById("projects-list")
+
 
 // event listeners
 addBtn.addEventListener("click", function() {
     if (!projnameInputEl.value) {
         return
     }
-    addProject(projnameInputEl.value)
+    addProjectToDB(projnameInputEl.value)
+    clearInputEl()
 })
 
 
 // functions
-function addProject(projname) {
-    projectsListEl.innerHTML += `<li>${projname}</li>`
-    
+function addProjectToDB(projname) {
+    push(projectsInDB, projname)
 }
+
+function clearInputEl() {
+    projnameInputEl.value = ""
+}
+
+function addProjectsToListEl(projnames) {
+    let strHTML = ""
+    for (let i = 0; i < projnames.length; i++) {
+        let currProjname = projnames[i]
+        strHTML += `<li>${currProjname}</li>`
+    }
+    projectsListEl.innerHTML = strHTML
+}
+
+function clearProjectListEl() {
+    projectsListEl.innerHTML = ""
+}
+
